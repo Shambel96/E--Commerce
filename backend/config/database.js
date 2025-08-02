25679a6d5da34e4ca73303f44ea64000cadb4a3d
Shambel96 <shambeldechu@gmail.com>
2025-08-02 13:00:00 +0300

    docs: added db connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,14 @@
 // To connect with our database
-// CommonJs
 const mongoose = require("mongoose");
+require("dotenv").config(); // Loads the .env file
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully!");
   } catch (err) {
-    console.log("Error connecting to the database:", err);
+    console.log("Error connecting to the database:", err.message);
+    process.exit(1);
   }
 };
 
