7a87ac1e2b10647ad93dd4645f61128636b6c061
Shambel Dechu <shambeldechu@gmail.com>
2026-03-05 13:59:41 +0300

    chore: add numreviews and description

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,7 +2,24 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
       type: String,
       required: true,
     },
@@ -10,14 +27,25 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
     price: {
       type: Number,
       required: true,
       default: 0,
     },
-    inStock: {
-      type: Boolean,
+    countInStock: {
+      type: Number,
       required: true,
+      default: 0,
     },
   },
   {
